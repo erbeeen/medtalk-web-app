7e08e8436de62216e309de8537da2fd2f0e6e030
erbeeen <irvinkierpresto26@gmail.com>
2025-04-22 14:27:05 +0800

    feat(WIP: fda-medicine): add FDAMedicineType

--- backend/models/fda-medicine.model.ts
+++ backend/models/fda-medicine.model.ts
@@ -1,67 +1,52 @@
 import mongoose from "mongoose";
 
-const fdaMedicine = new mongoose.Schema({
-  "Product Information": {
-    type: String,
-    required: false,
-  },
-  "Registration Number": {
-    type: String,
-    required: false,
-  },
+export type FDAMedicineType = {
+  "Product Information"?: string;
+  "Registration Number"?: string;
+  "Generic Name": string;
+  "Brand Name": string;
+  "Dosage Strength"?: string;
+  "Dosage Form"?: string;
+  Classification?: string;
+  "Pharmacologic Category"?: string;
+  Packaging?: string;
+  Manufacturer?: string;
+  "Country of Origin"?: string;
+  Trader?: string;
+  Imorter?: string;
+  Distributor?: string;
+  "Expiry Date"?: string; // TODO: Convert into date type
+};
+
+export type FDAMedicineDocument = FDAMedicineType & mongoose.Document;
+
+const fdaMedicineSchema = new mongoose.Schema({
+  "Product Information": String,
+  "Registration Number": String,
   "Generic Name": {
     type: String,
-    required: false,
+    required: true,
   },
   "Brand Name": {
     type: String,
     required: true,
   },
-  "Dosage Strength": {
-    type: String,
-    required: false,
-  },
-  "Dosage Form": {
-    type: String,
-    required: false,
-  },
-  Classification: {
-    type: String,
-    required: false,
-  },
-  "Pharmacologic Category": {
-    type: String,
-    required: false,
-  },
-  Packaging: {
-    type: String,
-    required: false,
-  },
-  Manufacturer: {
-    type: String,
-    required: false,
-  },
-  "Country of Origin": {
-    type: String,
-    required: false,
-  },
-  Trader: {
-    type: String,
-    required: false,
-  },
-  Importer: {
-    type: String,
-    required: false,
-  },
-  Distributor: {
-    type: String,
-    required: false,
-  },
-  "Expiry Date": {
-    type: String,
-    required: false,
-  },
+  "Dosage Strength": String,
+  "Dosage Form": String,
+  Classification: String,
+  "Pharmacologic Category": String,
+  Packaging: String,
+  Manufacturer: String,
+  "Country of Origin": String,
+  Trader: String,
+  Importer: String,
+  Distributor: String,
+  "Expiry Date": String, // TODO: Convert into date type
 });
 
-const FDAMedicine = mongoose.model("fda_medicine_database", fdaMedicine, "fda_medicine_database");
+const FDAMedicine = mongoose.model(
+  "fda_medicine_database",
+  fdaMedicineSchema,
+  "fda_medicine_database",
+);
 export default FDAMedicine;
