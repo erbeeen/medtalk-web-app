5713a0ccd872bb0323d908801abd98c526329e5e
erbeeen <irvinkierpresto26@gmail.com>
2025-04-22 14:24:30 +0800

    feat(WIP: doh medicine search): create doh medicine schema

---
+++ backend/models/doh-medicine.model.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+export type DOHMedicineType = {
+  "Level 1"?: string;
+  "Level 2"?: string;
+  "Level 3"?: string;
+  "Level 4"?: string;
+  Molecule: string;
+  Route?: string;
+  "Technical Specification"?: string;
+};
+
+export type DOHMedicineDocument = DOHMedicineType & mongoose.Document;
+
+const dohMedicineSchema = new mongoose.Schema({
+  "Level 1": String,
+  "Level 2": String,
+  "Level 3": String,
+  "Level 4": String,
+  Molecule: {
+    type: String,
+    required: true,
+  },
+  Route: String,
+  "Technical Specification": String,
+});
+
+const DOHMedicine = mongoose.model(
+  "doh_medicine_database",
+  dohMedicineSchema,
+  "doh_medicine_database",
+);
+export default DOHMedicine;
