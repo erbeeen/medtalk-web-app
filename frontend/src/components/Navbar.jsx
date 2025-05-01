41b0c5428182ee09556d70140ea9f048b93184ab
erbeeen <irvinkierpresto26@gmail.com>
2025-05-02 02:34:17 +0800

    frontend navbar: links styling

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,17 +2,26 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav id="navbar">
+    <nav>
       <div className="user-info flex-column nowrap align-center">
         <div className="profile-pic" />
-        <h3>Username</h3>
-        <h4>Role</h4>
+        <div id="navusername">Irvin</div>
+        <div id="navrole">Admin</div>
       </div>
+      <hr />
       <ul id="navbar-links" className="p-0 flex-column nowrap align-center">
-        <li><Link to="/">Dashboard</Link></li>
-        <li><Link to="/users">Users</Link></li>
-        <li><Link to="/medicine">Medicine</Link></li>
-        <li><Link to="/schedules">Schedules</Link></li>
+        <div className="navlink-container">
+          <li><Link className="navlink" to="/">Dashboard</Link></li>
+        </div>
+        <div className="navlink-container">
+          <li><Link className="navlink" to="/users">Users</Link></li>
+        </div>
+        <div className="navlink-container">
+          <li><Link className="navlink" to="/medicine">Medicine</Link></li>
+        </div>
+        <div className="navlink-container">
+          <li><Link className="navlink" to="/schedules">Schedules</Link></li>
+        </div>
       </ul>
     </nav>
   );
