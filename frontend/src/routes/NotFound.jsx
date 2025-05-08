ece87e9a6710d371ae11ebf034c71c4db622fcfb
erbeeen <irvinkierpresto26@gmail.com>
2025-05-09 02:32:01 +0800

    frontend: add not found page

---
+++ frontend/src/routes/NotFound.jsx
@@ -0,0 +1,7 @@
+export default function NotFoundRoute () {
+  return (
+    <div className="h-full w-full flex flex-row justify-center items-center">
+      <h1>404 Not Found</h1>
+    </div>
+  );
+}
