bb9c95c04779b6be9a73fbd3f96dc6404fd548c7
erbeeen <irvinkierpresto26@gmail.com>
2025-05-15 02:42:15 +0800

    frontend: make content area rounded and add other routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,32 @@
-import MedicineRoute from './routes/Medicine';
-import UsersRoute from './routes/Users';
-import ScheduleRoute from './routes/Schedules';
+import AdminsRoute from './routes/Admins';
 import HomeRoute from './routes/Home';
-// import Login from './routes/Login';
-import Sidebar from './components/Sidebar';
-import { Routes, Route } from 'react-router-dom';
-import './App.css';
+import MedicineRoute from './routes/Medicine';
 import NotFoundRoute from './routes/NotFound';
+import { Routes, Route } from 'react-router-dom';
+import ScheduleRoute from './routes/Schedules';
+import Sidebar from './components/Sidebar';
+import UsersRoute from './routes/Users';
+// import Login from './routes/Login';
+import './App.css';
 
 function App() {
   return (
     <>
       <div id="main" className="flex flex-row h-screen bg-light dark:bg-dark text-light-text dark:text-dark-text">
         <Sidebar />
-        <div className="h-screen w-full flex flex-col">
-          <div id="content-area" className="h-full w-full p-4 flex flex-col justify-start align-center gap-10">
-            <Routes>
-              <Route path="/" element={<HomeRoute />} />
-              <Route path="/users" element={<UsersRoute />} />
-              <Route path="/medicine" element={<MedicineRoute />} />
-              <Route path="/schedules" element={<ScheduleRoute />} />
-              <Route path="*" element={<NotFoundRoute />} />
-            </Routes>
-          </div>
+        <div className="h-screen w-full flex flex-col gap-0 bg-gray-700/40">
+          <div className="h-5 w-full bg-light-800/5"></div>
+            <div id="content-area" className="h-full w-[99%] p-4 flex flex-col justify-start align-center gap-10 border-2 border-white/8 rounded-2xl bg-dark text-dark-text/95">
+              <Routes>
+                <Route path="/" element={<HomeRoute />} />
+                <Route path="/users" element={<UsersRoute />} />
+                <Route path="/admins" element={<AdminsRoute />} />
+                <Route path="/medicine" element={<MedicineRoute />} />
+                <Route path="/schedules" element={<ScheduleRoute />} />
+                <Route path="*" element={<NotFoundRoute />} />
+              </Routes>
+            </div>
+          <div className="self-end h-5 w-full"></div>
         </div>
       </div>
     </>
--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,34 +1,40 @@
 import { Link } from "react-router-dom";
 import { MdDashboard } from "react-icons/md";
-import { FaUser, FaPills, FaCalendarAlt } from "react-icons/fa";
+import { FaUser, FaPills, FaCalendarAlt, FaUserEdit } from "react-icons/fa";
 
 export default function Sidebar() {
   return (
     // <div className="sidebar bg-light-800/5 dark:bg-dark-800/50 text-light-text dark:text-dark-text border-r dark:border-[#ffffff1a] border-dark/20">
-    <div className="sidebar bg-light-800/5 dark:bg-gray-700/40 text-light-text dark:text-dark-text border-r dark:border-[#ffffff1a]/30 border-dark/20">
-      <Link to="/" className="sidebar-link hover:bg-800 dark:hover:bg-dark-800">
+    <div className="sidebar bg-light-800/5 dark:bg-gray-700/40 font-open-sans text-light-text dark:text-dark-text">
+      <Link to="/" className="sidebar-link hover:bg-800 dark:hover:bg-primary-dark/20">
         <div className="sidebar-icon">
           <MdDashboard size="1.2rem" />
         </div>
-        <span className="w-8/12">Dashboard</span>
+        <span className="hidden lg:w-8/12 lg:block">Dashboard</span>
       </Link>
-      <Link to="/users" className="sidebar-link hover:bg-800 dark:hover:bg-dark-800">
+      <Link to="/users" className="sidebar-link hover:bg-800 dark:hover:bg-primary-dark/20">
         <div className="sidebar-icon">
           <FaUser size="1.2rem"/>
         </div>
         <span className="w-8/12">Users</span>
       </Link>
-      <Link to="/medicine" className="sidebar-link hover:bg-800 dark:hover:bg-dark-800">
+      <Link to="/admins" className="sidebar-link hover:bg-800 dark:hover:bg-primary-dark/20">
+        <div className="sidebar-icon">
+          <FaUserEdit size="1.5rem" />
+        </div>
+        <span className="w-8/12">Admins</span>
+      </Link>
+      <Link to="/medicine" className="sidebar-link hover:bg-800 dark:hover:bg-primary-dark/20">
         <div className="sidebar-icon">
           <FaPills size="1.2rem"/>
         </div>
-        <span className="w-8/12">Medicine</span>
+        <span className="w-8/12">Medicines</span>
       </Link>
-      <Link to="/schedules" className="sidebar-link hover:bg-800 dark:hover:bg-dark-800">
+      <Link to="/schedules" className="sidebar-link hover:bg-800 dark:hover:bg-primary-dark/20">
         <div className="sidebar-icon">
           <FaCalendarAlt size="1.2rem"/>
         </div>
-        <span className="w-8/12">Schedule</span>
+        <span className="w-8/12">Schedules</span>
       </Link>
     </div>
   );
