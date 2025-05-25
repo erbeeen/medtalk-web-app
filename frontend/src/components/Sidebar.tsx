e37ad6b4908b2ee1b6ad4b03545cea189a795756
erbeeen <irvinkierpresto26@gmail.com>
2025-05-26 00:41:51 +0800

    frontend(sidebar): change background and make mobile responsive

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -4,32 +4,32 @@ import { FaUser, FaPills, FaCalendarAlt, FaUserEdit } from "react-icons/fa";
 
 export default function Sidebar() {
   return (
-    <div className="sidebar bg-primary-dark/30 dark:bg-gray-700/40 text-light-text dark:text-dark-text">
-      <Link to="/" className="sidebar-link hover:bg-primary-800/30 hover:text-dark-text dark:hover:bg-primary-dark/20">
+    <div className="sidebar bg-gray-700/40 ">
+      <Link to="/" className="sidebar-link hover:bg-primary-800/20 hover:text-dark-text dark:hover:bg-primary-dark/50">
         <div className="sidebar-icon">
           <MdDashboard size="1.2rem" />
         </div>
-        <span className="hidden lg:w-8/12 lg:block">Dashboard</span>
+        <span className="w-8/12">Dashboard</span>
       </Link>
-      <Link to="/users" className="sidebar-link hover:bg-800 dark:hover:bg-primary-dark/20">
+      <Link to="/users" className="sidebar-link hover:bg-800 dark:hover:bg-primary-dark/50">
         <div className="sidebar-icon">
           <FaUser size="1.2rem"/>
         </div>
         <span className="w-8/12">Users</span>
       </Link>
-      <Link to="/admins" className="sidebar-link hover:bg-800 dark:hover:bg-primary-dark/20">
+      <Link to="/admins" className="sidebar-link hover:bg-800 dark:hover:bg-primary-dark/50">
         <div className="sidebar-icon">
           <FaUserEdit size="1.5rem" />
         </div>
         <span className="w-8/12">Admins</span>
       </Link>
-      <Link to="/medicine" className="sidebar-link hover:bg-800 dark:hover:bg-primary-dark/20">
+      <Link to="/medicine" className="sidebar-link hover:bg-800 dark:hover:bg-primary-dark/50">
         <div className="sidebar-icon">
           <FaPills size="1.2rem"/>
         </div>
         <span className="w-8/12">Medicines</span>
       </Link>
-      <Link to="/schedules" className="sidebar-link hover:bg-800 dark:hover:bg-primary-dark/20">
+      <Link to="/schedules" className="sidebar-link hover:bg-800 dark:hover:bg-primary-dark/50">
         <div className="sidebar-icon">
           <FaCalendarAlt size="1.2rem"/>
         </div>
