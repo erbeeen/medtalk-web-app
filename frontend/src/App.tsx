c565560153f0dee194ccb43d23ed0e5e57fa8c72
erbeeen <irvinkierpresto26@gmail.com>
2025-05-26 00:42:33 +0800

    frontend(base): new additions to layout, css for mobile responsiveness

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,7 +13,7 @@ import { useRef } from 'react';
 function App() {
   const mainContentRef = useRef<HTMLDivElement>(null);
 
-  const scrollToTop = async () => {
+  const scrollToTop = () => {
     if (mainContentRef.current) {
       mainContentRef.current.scroll({
         top: 0,
@@ -26,7 +26,7 @@ function App() {
   return (
     <>
       <div id="main"
-        className="h-screen flex bg-light dark:bg-dark 
+        className="h-screen flex bg-light dark:bg-dark
         text-light-text dark:text-dark-text">
         <Sidebar />
         <div
@@ -34,13 +34,13 @@ function App() {
           id="content-area"
           className="w-full p-4 flex flex-col flex-1 
           overflow-y-auto justify-start align-center gap-10 
-          bg-light dark:bg-[#181924] text-light-text dark:text-dark-text/95">
+          bg-light dark:bg-dark text-light-text dark:text-dark-text/95">
           <Routes>
             <Route path="/" element={<HomeRoute />} />
             <Route path="/users" element={<UsersRoute scrollToTop={scrollToTop} />} />
-            <Route path="/admins" element={<AdminsRoute />} />
-            <Route path="/medicine" element={<MedicineRoute />} />
-            <Route path="/schedules" element={<ScheduleRoute />} />
+            <Route path="/admins" element={<AdminsRoute scrollToTop={scrollToTop} />} />
+            <Route path="/medicine" element={<MedicineRoute scrollToTop={scrollToTop} />} />
+            <Route path="/schedules" element={<ScheduleRoute scrollToTop={scrollToTop} />} />
             <Route path="*" element={<NotFoundRoute />} />
           </Routes>
         </div>
