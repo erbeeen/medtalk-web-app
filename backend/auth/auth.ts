e06fef060dbfca04a98d2ad9d8fe32e054b48faf
erbeeen <irvinkierpresto26@gmail.com>
2025-06-03 02:25:31 +0800

    backend(auth & logger): make some processes asynchronous

--- backend/auth/auth.ts
+++ backend/auth/auth.ts
@@ -47,43 +47,80 @@ export function generateRefreshToken(
   }
 }
 
-export function refreshAccessToken(
+export async function refreshAccessToken(
   token: string,
-): [string | null, boolean, Error | null] {
-  jwt.verify(
-    token,
-    process.env.SECRET_REFRESH_TOKEN,
-    (err, user: CustomJwtPayload) => {
-      if (err) {
-        return [null, false, null];
-      } else {
-        const [accessToken, accessTokenErr] = generateAccessToken(
-          user.id,
-          user.username,
-          user.role
-        );
-        if (accessTokenErr) {
-          return [null, false, accessTokenErr];
+): Promise<[string | null, boolean, Error | null]> {
+  try {
+    const user: CustomJwtPayload = await new Promise((resolve, reject) => {
+      jwt.verify(token, process.env.SECRET_REFRESH_TOKEN, (err, decoded) => {
+        if (err) {
+          console.log("\nrefreshAccessToken jwt.verify err: ", err);
+          return reject(err);
         }
-        return [accessToken, true, null];
-      }
-    },
-  );
-  return ["", false, Error("jwt.verify did not function")];
+        resolve(decoded as CustomJwtPayload);
+      });
+    });
+
+    const [accessToken, accessTokenErr] = generateAccessToken(
+      user.id,
+      user.username,
+      user.role,
+    );
+
+    if (accessTokenErr) {
+      console.log(
+        "\nrefreshAccessToken generateAccessToken error: ",
+        accessTokenErr,
+      );
+    }
+
+    return [accessToken, true, null];
+  } catch (err) {
+    console.log("refreshAccessToken caught error: ", err);
+    return [null, false, err];
+  }
+
+  // jwt.verify(
+  //   token,
+  //   process.env.SECRET_REFRESH_TOKEN,
+  //   (err, user: CustomJwtPayload) => {
+  //     if (err) {
+  //       console.log("\nrefreshAccessToken jwt.verify err: ", err);
+  //       return [null, false, null];
+  //     }
+  //     const [accessToken, accessTokenErr] = generateAccessToken(
+  //       user.id,
+  //       user.username,
+  //       user.role,
+  //     );
+  //     if (accessTokenErr) {
+  //       return [null, false, accessTokenErr];
+  //     }
+  //     return [accessToken, true, null];
+  //   },
+  // );
+  // return ["", false, Error("refreshAccessToken jwt.verify did not function")];
 }
 
-export function validateRefreshToken(
+export async function validateRefreshToken(
   token: string,
-): [boolean | null, Error | null] {
-  jwt.verify(
-    token,
-    process.env.SECRET_REFRESH_TOKEN,
-    (err, _user: CustomJwtPayload) => {
-      if (err) {
-        return [false, null];
-      }
-      return [true, null];
-    },
-  );
-  return [false, Error("jwt.verify did not run")];
+): Promise<[boolean | null, Error | null]> {
+  try {
+    const isTokenValid: boolean = await new Promise((resolve, _reject) => {
+      jwt.verify(
+        token,
+        process.env.SECRET_REFRESH_TOKEN,
+        (err, _user: CustomJwtPayload) => {
+          if (err) {
+            resolve(false);
+          }
+          resolve(true);
+        },
+      );
+    });
+    return [isTokenValid, null];
+  } catch (err) {
+    console.log("validateRefreshToken caught error: ", err);
+    return [null, err];
+  }
 }
