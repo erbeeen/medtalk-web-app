967d6a5518d665c790c371a0bc9da9bfba95c51b
erbeeen <irvinkierpresto26@gmail.com>
2025-06-27 04:12:27 +0800

    refactor(verify email): revert changes

--- backend/controllers/user.controller.ts
+++ backend/controllers/user.controller.ts
@@ -162,7 +162,10 @@ export default class UserController {
           // not the API route. The frontend route will call the API route. Use only for testing if this function works
 
           const info = await this.emailTransporter.sendMail({
-            sender: '"MedTalk" <medtalk.test.ph@gmail.com>',
+            from: {
+              name: "MedTalk",
+              address: process.env.MAIL_USERNAME,
+            },
             to: result.email,
             subject: "Account Verification - MedTalk",
             text: `Thank you for signing up at MedTalk! Click the link to verify your account. https://medtalk-webapp-122bcbf0f96e.herokuapp.com/api/users/verify/?id=${result._id}`,
