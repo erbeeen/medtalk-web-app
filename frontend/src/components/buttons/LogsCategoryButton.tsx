c685f93481aff53fd122c10de9d9c6af3c9c2bd5
erbeeen <irvinkierpresto26@gmail.com>
2025-09-19 02:45:53 +0800

    feat(system logs frontend): add filters

--- frontend/src/components/buttons/LogsCategoryButton.tsx
+++ frontend/src/components/buttons/LogsCategoryButton.tsx
@@ -1,10 +1,23 @@
+import { useState, useEffect } from "react";
 type LogsCategoryButtonProps = {
   label: string;
+  updateFilters: (filterName: string, isActive: Boolean) => void;
 }
 
-export default function LogsCategoryButton({ label }: LogsCategoryButtonProps) {
+export default function LogsCategoryButton({ label, updateFilters }: LogsCategoryButtonProps) {
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    updateFilters(label, isActive);
+  }, [isActive]);
+
   return (
-    <div className="logs-category border-gray-700/80 bg-gray-700/40 hover:bg-gray-700/80">
+    <div
+      className={`logs-category ${!isActive ? 'bg-gray-700/40 hover:bg-gray-700/80' : 'bg-gray-700/80'}`}
+      onClick={() => {
+        setIsActive(prev => !prev);
+      }}
+    >
       <button className="text-xs cursor-pointer">{label}</button>
     </div>
   );
--- frontend/src/routes/SystemLogs.tsx
+++ frontend/src/routes/SystemLogs.tsx
@@ -3,10 +3,37 @@ import LogsCategoryButton from "../components/buttons/LogsCategoryButton";
 import { type SystemLogType } from "../types/systemlogs";
 import SystemLogCard from "../components/SystemLogCard";
 
-// TODO: Filters
+// TODO: 
 // Pagination (optional)
 export default function SystemLogsRoute() {
   const [logs, setLogs] = useState<Array<SystemLogType>>([]);
+  const [filteredLogs, setFilteredLogs] = useState<Array<SystemLogType>>([]);
+  const [filters, setFilters] = useState<Array<String>>([]);
+
+  const updateFilters = (filterName: string, isActive: Boolean) => {
+    if (isActive) {
+      setFilters(prev => [...prev, filterName]);
+    } else {
+      const updatedFilters = filters.filter(filter => filter !== filterName);
+      setFilters(updatedFilters);
+    }
+  };
+
+  useEffect(() => {
+    if (filters.length === 0) {
+      setFilteredLogs(logs);
+      return;
+    }
+    const updatedLogs = logs.filter((log) => {
+      return filters.some((filter) => {
+        if (filter === "error") {
+          return log.level == filter
+        }
+        return log.category == filter
+      });
+    });
+    setFilteredLogs(updatedLogs);
+  }, [filters]);
 
   const fetchData = async () => {
     try {
@@ -23,12 +50,14 @@ export default function SystemLogsRoute() {
       }
       const data = await response.json();
       setLogs(data.data);
+      setFilteredLogs(data.data);
     } catch (err) {
       console.error("fetching logs failed: ", err);
     }
   }
 
   useEffect(() => {
+    document.title = "Logs | MedTalk"
     fetchData();
   }, []);
   return (
@@ -36,15 +65,16 @@ export default function SystemLogsRoute() {
       <h1 className="self-start text-2xl font-bold">System Logs</h1>
 
       <div className="w-full flex flex-row justify-start gap-3">
-        <LogsCategoryButton label="authentication" />
-        <LogsCategoryButton label="user-management" />
-        <LogsCategoryButton label="medicine-management" />
-        <LogsCategoryButton label="schedule-management" />
-        <LogsCategoryButton label="error" />
+        <LogsCategoryButton label="authentication" updateFilters={updateFilters} />
+        <LogsCategoryButton label="user-management" updateFilters={updateFilters} />
+        <LogsCategoryButton label="admin-management" updateFilters={updateFilters} />
+        <LogsCategoryButton label="medicine-management" updateFilters={updateFilters} />
+        <LogsCategoryButton label="schedule-management" updateFilters={updateFilters} />
+        <LogsCategoryButton label="error" updateFilters={updateFilters} />
       </div>
       <div className="w-full pr-5 self-start">
-        {logs.map((log) => {
-          return (<SystemLogCard log={log} />);
+        {filteredLogs.map((log) => {
+          return (<SystemLogCard key={log._id} log={log} />);
         })}
       </div>
     </div>
