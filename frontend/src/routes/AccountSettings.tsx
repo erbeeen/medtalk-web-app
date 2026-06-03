47b40d0a1770c0254ad9cbb3994dcf53077c2024
erbeeen <irvinkierpresto26@gmail.com>
2026-06-03 16:49:48 +0800

    refactor(account settings): add loading state

--- frontend/src/routes/AccountSettings.tsx
+++ frontend/src/routes/AccountSettings.tsx
@@ -23,6 +23,7 @@ export default function AccountSettingsRoute() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [loading, setLoading] = useState(false);
   const [isUpdateProfileLoading, setIsUpdateProfileLoading] = useState(false);
   const [isUpdatePasswordLoading, setIsUpdatePasswordLoading] = useState(false);
   const [updateProfileMessage, setUpdateProfileMessage] = useState<string | null>(null);
@@ -34,6 +35,7 @@ export default function AccountSettingsRoute() {
     const fetchProfile = async () => {
       if (!user?.id) return;
       try {
+        setLoading(true);
         const res = await fetch(`/api/users/?id=${user?.id}`, {
           mode: "cors",
           method: "GET",
@@ -56,6 +58,8 @@ export default function AccountSettingsRoute() {
       } catch {
         addToast("Failed to get user data. Try again later", { type: "error" });
         // no-op
+      } finally {
+        setLoading(false);
       }
     };
     fetchProfile();
@@ -152,7 +156,7 @@ export default function AccountSettingsRoute() {
   };
 
   return (
-    <div className="w-full flex justify-center items-start">
+    <div className="w-full h-full flex flex-col justify-start items-center">
       <div className="w-full max-w-5xl flex flex-col items-center gap-8 rounded-xl p-8 ">
         <div className="w-full flex flex-col items-center gap-3 text-center">
           <FaUserCircle className="text-gray-500 dark:text-gray-300" size={120} />
@@ -162,71 +166,78 @@ export default function AccountSettingsRoute() {
           </p>
         </div>
 
-        <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-8">
-          <form onSubmit={handleUpdateProfile} className="w-full">
-            <div className="h-full px-6 md:px-10 py-6 flex flex-col justify-center gap-5 bg-gray-200 dark:bg-gray-700/30 rounded-lg">
-              <h2 className="text-xl font-bold mb-2">Profile</h2>
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                <div className="flex flex-col gap-1">
-                  <label htmlFor="firstName" className="pl-1">First Name</label>
-                  <input id="firstName" className="modal-input" value={firstName} onChange={(e) => setFirstName(e.target.value)} required />
-                </div>
-                <div className="flex flex-col gap-1">
-                  <label htmlFor="lastName" className="pl-1">Last Name</label>
-                  <input id="lastName" className="modal-input" value={lastName} onChange={(e) => setLastName(e.target.value)} required />
-                </div>
-              </div>
-              <div className="flex flex-col gap-1">
-                <label htmlFor="email" className="pl-1">Email Address</label>
-                <input id="email" type="email" className="modal-input" value={email} onChange={(e) => setEmail(e.target.value)} required />
-              </div>
-              <div className="flex flex-col gap-1">
-                <label htmlFor="username" className="pl-1">Username</label>
-                <input id="username" className="modal-input" value={username} onChange={(e) => setUsername(e.target.value)} required />
-              </div>
-              <div className="flex justify-center items-center text-delete text-sm min-h-5">
-                {updateProfileMessage}
-              </div>
-              <SubmitButton isLoading={isUpdateProfileLoading}>Update</SubmitButton>
-            </div>
-          </form>
+        {loading ? (
+          <div className="flex-1 flex justify-center items-center">
+            <div className="spinner size-10" />
+          </div>
+        ) : (
 
-          <form onSubmit={handleUpdatePassword} className="w-full">
-            <div className="h-full px-6 md:px-10 py-6 flex flex-col justify-center gap-5 bg-gray-200 dark:bg-gray-700/30 rounded-lg">
-              <h2 className="text-xl font-bold mb-2">Change Password</h2>
-              <div className="flex flex-col gap-1">
-                <label htmlFor="password" className="pl-1">New Password</label>
-                <input
-                  type="password"
-                  id="password"
-                  name="password"
-                  className="modal-input"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  required
-                  minLength={8}
-                />
+          <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-8">
+            <form onSubmit={handleUpdateProfile} className="w-full">
+              <div className="h-full px-6 md:px-10 py-6 flex flex-col justify-center gap-5 bg-gray-200 dark:bg-gray-700/30 rounded-lg">
+                <h2 className="text-xl font-bold mb-2">Profile</h2>
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                  <div className="flex flex-col gap-1">
+                    <label htmlFor="firstName" className="pl-1">First Name</label>
+                    <input id="firstName" className="modal-input" value={firstName} onChange={(e) => setFirstName(e.target.value)} required />
+                  </div>
+                  <div className="flex flex-col gap-1">
+                    <label htmlFor="lastName" className="pl-1">Last Name</label>
+                    <input id="lastName" className="modal-input" value={lastName} onChange={(e) => setLastName(e.target.value)} required />
+                  </div>
+                </div>
+                <div className="flex flex-col gap-1">
+                  <label htmlFor="email" className="pl-1">Email Address</label>
+                  <input id="email" type="email" className="modal-input" value={email} onChange={(e) => setEmail(e.target.value)} required />
+                </div>
+                <div className="flex flex-col gap-1">
+                  <label htmlFor="username" className="pl-1">Username</label>
+                  <input id="username" className="modal-input" value={username} onChange={(e) => setUsername(e.target.value)} required />
+                </div>
+                <div className="flex justify-center items-center text-delete text-sm min-h-5">
+                  {updateProfileMessage}
+                </div>
+                <SubmitButton isLoading={isUpdateProfileLoading}>Update</SubmitButton>
               </div>
-              <div className="flex flex-col gap-1">
-                <label htmlFor="confirmPassword" className="pl-1">Confirm Password</label>
-                <input
-                  type="password"
-                  id="confirmPassword"
-                  name="confirmPassword"
-                  className="modal-input"
-                  value={confirmPassword}
-                  onChange={(e) => setConfirmPassword(e.target.value)}
-                  required
-                  minLength={8}
-                />
+            </form>
+
+            <form onSubmit={handleUpdatePassword} className="w-full">
+              <div className="h-full px-6 md:px-10 py-6 flex flex-col justify-center gap-5 bg-gray-200 dark:bg-gray-700/30 rounded-lg">
+                <h2 className="text-xl font-bold mb-2">Change Password</h2>
+                <div className="flex flex-col gap-1">
+                  <label htmlFor="password" className="pl-1">New Password</label>
+                  <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    className="modal-input"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                    minLength={8}
+                  />
+                </div>
+                <div className="flex flex-col gap-1">
+                  <label htmlFor="confirmPassword" className="pl-1">Confirm Password</label>
+                  <input
+                    type="password"
+                    id="confirmPassword"
+                    name="confirmPassword"
+                    className="modal-input"
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                    required
+                    minLength={8}
+                  />
+                </div>
+                <div className="flex justify-center items-center text-delete text-sm min-h-5">
+                  {updatePasswordMessage}
+                </div>
+                <SubmitButton isLoading={isUpdatePasswordLoading}>Update password</SubmitButton>
               </div>
-              <div className="flex justify-center items-center text-delete text-sm min-h-5">
-                {updatePasswordMessage}
-              </div>
-              <SubmitButton isLoading={isUpdatePasswordLoading}>Update password</SubmitButton>
-            </div>
-          </form>
-        </div>
+            </form>
+          </div>
+        )}
       </div>
     </div>
   );
